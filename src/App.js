5965d64ffaf1d2f3cc5ca0d05a80f8cbe55722bb
Patricia Silva <psilva@imaginarycloud.com>
2020-11-06 12:35:18 +0000

    add materialui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@material-ui/core/Button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button variant="outlined" color="primary">
+          Hello World
+        </Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
