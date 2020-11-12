35ba80f03ce6c8488e511597c5c89c082bd4e35b
Patricia Silva <psilva@imaginarycloud.com>
2020-11-12 11:46:59 +0000

    create the base structure of the rest of the page with grid elements

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Autocomplete from "@material-ui/lab/Autocomplete";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 import React, { useState } from "react";
 
 import top100Films from "./markers.js";
@@ -27,6 +29,21 @@ function App() {
           )}
           onChange={(event, value) => setLocation(value.marker)}
         />
+        <Grid container spacing={3}>
+          <Grid item xs={4}>
+            <Paper>xs=4</Paper>
+          </Grid>
+          <Grid item xs={8}>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <Paper>xs=12</Paper>
+              </Grid>
+              <Grid item xs={12}>
+                <Paper>xs=12</Paper>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
       </header>
     </div>
   );
