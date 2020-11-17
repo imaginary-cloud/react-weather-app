617626115aa99e90ddfcb387e179058366288b14
Patricia Silva <psilva@imaginarycloud.com>
2020-11-17 17:52:32 +0000

    add api request to obtain location information

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import TextField from "@material-ui/core/TextField";
 import Autocomplete from "@material-ui/lab/Autocomplete";
+import axios from "axios";
+import { useQuery } from "react-query";
 
 import top100Films from "./markers.js";
 
 function SearchBar(props) {
+  const [input, setInput] = useState("");
+
+  const { data, isLoading, isError, error } = useQuery(
+    ["getLocationInfo", input],
+    () => {
+      if (input) {
+        return axios
+          .get(
+            `http://localhost:8010/proxy/api/location/search/?query=${input}`
+          )
+          .then(({ data } = {}) => data);
+      } else return [];
+    }
+  );
+
   return (
     <Autocomplete
       id="combo-box-demo"
       fullWidth
-      options={top100Films}
-      getOptionLabel={option => option.marker}
+      options={!isLoading ? data : []}
+      getOptionLabel={option => option.title}
       renderInput={params => (
-        <TextField {...params} label="Select a Location" variant="outlined" />
+        <TextField
+          {...params}
+          label="Select a Location"
+          variant="outlined"
+          onChange={(event, value) => setInput(event.target.value)}
+        />
       )}
-      onChange={(event, value) => props.onChange(value.marker)}
+      onChange={(event, value) => props.onChange(value)}
     />
   );
 }
