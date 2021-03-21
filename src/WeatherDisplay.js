ce2e28a7922abb6ac056f28b39653bb283b0b505
Patricia Silva <psilva@imaginarycloud.com>
2021-03-21 23:44:35 +0000

    Create structure of the page

---
+++ src/WeatherDisplay.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: "20px 40px"
+  }
+}));
+
+export default function WeatherDisplay() {
+  const classes = useStyles();
+
+  return (
+    <Grid container spacing={3} className={classes.root}>
+      <Grid item xs={12}>
+        <Paper className={classes.paper}>xs=12</Paper>
+      </Grid>
+      <Grid item xs={12}>
+        <Paper className={classes.paper}>xs=12</Paper>
+      </Grid>
+      <Grid item xs={12}>
+        <Paper className={classes.paper}>xs=12</Paper>
+      </Grid>
+    </Grid>
+  );
+}
