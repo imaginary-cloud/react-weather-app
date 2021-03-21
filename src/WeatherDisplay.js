aacbaba088448b8b89747a8a4f2e71604b39695d
Patricia Silva <psilva@imaginarycloud.com>
2021-03-21 23:46:52 +0000

    Add weather app location title

--- src/WeatherDisplay.js
+++ src/WeatherDisplay.js
@@ -2,6 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -15,7 +16,9 @@ export default function WeatherDisplay() {
   return (
     <Grid container spacing={3} className={classes.root}>
       <Grid item xs={12}>
-        <Paper className={classes.paper}>xs=12</Paper>
+        <Typography variant="h3" align="center">
+          Lisbon
+        </Typography>
       </Grid>
       <Grid item xs={12}>
         <Paper className={classes.paper}>xs=12</Paper>
