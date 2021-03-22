43bd99f9e99c1a0f53620ea5cfb84810d1f26216
Patricia Silva <psilva@imaginarycloud.com>
2021-03-22 01:09:31 +0000

    replace full weather card structure

--- src/FullWeatherCard.js
+++ src/FullWeatherCard.js
@@ -1,57 +1,86 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
+import Grid from "@material-ui/core/Grid";
 import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   root: {
     minWidth: 275
-  },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)"
-  },
-  title: {
-    fontSize: 14
-  },
-  pos: {
-    marginBottom: 12
   }
 });
 
 export default function FullWeatherCard() {
   const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
 
   return (
     <Card className={classes.root}>
       <CardContent>
-        <Typography
-          className={classes.title}
-          color="textSecondary"
-          gutterBottom
-        >
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="h2">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+        <Grid container>
+          <Grid item xs={12}>
+            <Typography variant="h3" component="h3" align="center">
+              13 November 2020
+            </Typography>
+          </Grid>
+          <Grid item xs={3}>
+            <Typography variant="h4" component="h3" align="right">
+              15 ºC
+            </Typography>
+          </Grid>
+          <Grid item xs={3}>
+            <Typography variant="p" component="p" align="left">
+              Max - 16
+            </Typography>
+            <Typography variant="p" component="p" align="left">
+              Min - 10
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            {/* Icon space */}
+          </Grid>
+          <Grid item xs={4}>
+            <Typography variant="p" component="p" align="center">
+              Wind speed
+            </Typography>
+            <Typography variant="p" component="p" align="center">
+              5.7
+            </Typography>
+          </Grid>
+          <Grid item xs={4}>
+            <Typography variant="p" component="p" align="center">
+              Wind direction
+            </Typography>
+            <Typography variant="p" component="p" align="center">
+              96.7
+            </Typography>
+          </Grid>
+          <Grid item xs={4}>
+            <Typography variant="p" component="p" align="center">
+              Wind direction compass
+            </Typography>
+            <Typography variant="p" component="p" align="center">
+              East
+            </Typography>
+          </Grid>
+          <Grid item xs={4}>
+            <Typography variant="p" component="p" align="center">
+              Air pressure
+            </Typography>
+            <Typography variant="p" component="p" align="center">
+              1030
+            </Typography>
+          </Grid>
+          <Grid item xs={4} className={classes.pos}>
+            <Typography variant="p" component="p" align="center">
+              Humidity
+            </Typography>
+            <Typography variant="p" component="p" align="center">
+              70
+            </Typography>
+          </Grid>
+        </Grid>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
     </Card>
   );
 }
