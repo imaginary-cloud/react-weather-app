d67e12e6cc07a953ae5b610a9679c3daf8203550
Patricia Silva <psilva@imaginarycloud.com>
2021-03-22 00:25:27 +0000

    replace information on the weatherCard

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -10,48 +10,26 @@ const useStyles = makeStyles({
   root: {
     minWidth: 275
   },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)"
-  },
-  title: {
-    fontSize: 14
-  },
-  pos: {
-    marginBottom: 12
+  icon: {
+    height: "100px"
   }
 });
 
 export default function WeatherCard() {
   const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
 
   return (
     <Card className={classes.root}>
       <CardContent>
-        <Typography
-          className={classes.title}
-          color="textSecondary"
-          gutterBottom
-        >
-          Word of the Day
-        </Typography>
         <Typography variant="h5" component="h2">
-          be{bull}nev{bull}o{bull}lent
+          22 March
         </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
+        {/* image */}
+        <div className={classes.icon}></div>
+        <Typography variant="h5" component="h2">
+          18 ºC
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
     </Card>
   );
 }
