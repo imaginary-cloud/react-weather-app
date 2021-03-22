383b815979ecaacda3af6f1f36ec31ef7077b3b5
Patricia Silva <psilva@imaginarycloud.com>
2021-03-22 01:48:34 +0000

    create info display component to show each info on full weather card

---
+++ src/InfoDisplay.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles({
+  title: {
+    fontSize: 20
+  },
+  value: {
+    fontSize: 26
+  },
+  pos: {
+    marginBottom: 20
+  }
+});
+
+function InfoDisplay(props) {
+  const classes = useStyles();
+
+  return (
+    <Grid item xs={props.size} className={classes.pos}>
+      <Typography className={classes.title} component="p" align="center">
+        {props.title}
+      </Typography>
+      <Typography className={classes.value} component="p" align="center">
+        {props.value}
+      </Typography>
+    </Grid>
+  );
+}
+
+export default InfoDisplay;
