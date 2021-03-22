4bc83b838a7f6774d36cc4a8e7cf6d8cb0ec6cd0
Patricia Silva <psilva@imaginarycloud.com>
2021-03-22 01:43:40 +0000

    replace static data for real data form the weather api

--- src/WeatherDisplay.js
+++ src/WeatherDisplay.js
@@ -3,8 +3,9 @@ import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import WeatherCard from "./WeatherCard";
-import data from "./data/example_data_location_request.json";
 import FullWeatherCard from "./FullWeatherCard";
+import axios from "axios";
+import { useQuery } from "react-query";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -14,33 +15,60 @@ const useStyles = makeStyles(theme => ({
 
 export default function WeatherDisplay() {
   const classes = useStyles();
-  const [selectedData, setSelectedData] = useState(
-    data.consolidated_weather[0]
+  const [selectedData, setSelectedData] = useState([]);
+
+  const { data, isLoading, isError, error } = useQuery(
+    ["getLocationDetails", "742676"],
+    () => {
+      return axios
+        .get(`http://localhost:8010/proxy/api/location/742676/`)
+        .then(({ data } = {}) => {
+          setSelectedData(data.consolidated_weather[0]);
+          return data;
+        });
+    }
   );
 
-  const weatherdata = data.consolidated_weather.slice(0, 5).map(data => {
-    return (
-      <WeatherCard
-        key={data.id}
-        data={data}
-        setSelectedData={setSelectedData}
-      ></WeatherCard>
-    );
-  });
+  if (data && data.consolidated_weather) {
+    const weatherdata = data.consolidated_weather.slice(0, 5).map(data => {
+      return (
+        <WeatherCard
+          key={data.id}
+          data={data}
+          setSelectedData={setSelectedData}
+        ></WeatherCard>
+      );
+    });
 
-  return (
-    <Grid container spacing={3} className={classes.root}>
-      <Grid item xs={12}>
-        <Typography variant="h3" align="center">
-          Lisbon
+    return (
+      <Grid container spacing={3} className={classes.root}>
+        <Grid item xs={12}>
+          <Typography variant="h3" align="center">
+            Lisbon
+          </Typography>
+        </Grid>
+        <Grid container item xs={12} justify="space-between">
+          {weatherdata}
+        </Grid>
+        <Grid item xs={12}>
+          <FullWeatherCard selectedData={selectedData}></FullWeatherCard>
+        </Grid>
+      </Grid>
+    );
+  } else {
+    return (
+      <Grid
+        item
+        xs={12}
+        container
+        spacing={3}
+        className={classes.root}
+        justify="center"
+      >
+        <Typography variant="h3" component="h3" align="center">
+          Loading...
         </Typography>
       </Grid>
-      <Grid container item xs={12} justify="space-between">
-        {weatherdata}
-      </Grid>
-      <Grid item xs={12}>
-        <FullWeatherCard selectedData={selectedData}></FullWeatherCard>
-      </Grid>
-    </Grid>
-  );
+    );
+  }
 }
