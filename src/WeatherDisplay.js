d10e6da84ea11298fac580b32852063a6cb9b304
Patricia Silva <psilva@imaginarycloud.com>
2021-03-22 00:16:47 +0000

    create weather card from material ui card

--- src/WeatherDisplay.js
+++ src/WeatherDisplay.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+import WeatherCard from "./WeatherCard";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -21,7 +22,7 @@ export default function WeatherDisplay() {
         </Typography>
       </Grid>
       <Grid item xs={12}>
-        <Paper className={classes.paper}>xs=12</Paper>
+        <WeatherCard></WeatherCard>
       </Grid>
       <Grid item xs={12}>
         <Paper className={classes.paper}>xs=12</Paper>
